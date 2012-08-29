e70733603b9ac75d9f4c7f14d7328976bad8f190
David Xie <david.xie@me.com>
2012-08-29 21:24:47 +0800

    将views/user.py中的form移到了forms/__init__.py 个人认为, 有一个专门的forms还是比较好的

---
+++ website/scriptfan/forms/__init__.py
@@ -0,0 +1,80 @@
+#-*-coding:utf-8-*-
+from flask.ext import wtf, login
+
+class SigninForm(wtf.Form):
+    email = wtf.TextField('email', validators=[
+        wtf.Required(message=u'请填写电子邮件'), 
+        wtf.Email(message=u'无效的电子邮件')])
+    password = wtf.PasswordField('password', validators=[
+        wtf.Required(message=u'请填写密码'),
+        wtf.Length(min=5, max=20, message=u'密应应为5到20位字符')])
+    next = wtf.HiddenField('next')
+    remember = wtf.BooleanField('remember')
+    
+    openid_identifier = wtf.HiddenField('openid_identifier')
+    openid_provider = wtf.HiddenField('openid_provider')
+
+    def __init__(self, *args, **kargs):
+        wtf.Form.__init__(self, *args, **kargs)
+        self.user = None
+
+    def validate(self):
+        # 验证邮箱是否注册
+        if wtf.Form.validate(self):
+            user = get_user(email=self.email.data)
+            if not user:
+                self.email.errors.append(u'该邮箱尚未在本站注册')
+            elif not user.check_password(self.password.data):
+                self.password.errors.append(u'密码错误')
+            else:
+                self.user = user
+
+        return len(self.errors) == 0
+
+class SignupForm(wtf.Form):
+    email = wtf.TextField('email', validators=[
+        wtf.Required(message=u'请填写电子邮件'), 
+        wtf.Email(message=u'无效的电子邮件')])
+    nickname = wtf.TextField('nickname', validators=[
+        wtf.Required(message=u'请填写昵称'),
+        wtf.Length(min=2, max=20, message=u'昵称应为2到20字符')])
+    password = wtf.PasswordField('password', validators=[
+        wtf.Required(message=u'请填写密码'),
+        wtf.Length(min=5, max=20, message=u'密码应为5到20位字符')])
+    repassword = wtf.PasswordField('repassword', validators=[
+        wtf.Required(message=u'请填写确认密码'),
+        wtf.EqualTo('password', message=u'两次输入的密码不一致')])
+    next = wtf.HiddenField('next')
+
+    def __init__(self, *args, **kargs):
+        wtf.Form.__init__(self, *args, **kargs)
+        self.user = None
+
+    def validate(self):
+        wtf.Form.validate(self)
+
+        # 验证邮箱是否注册
+        if not self.email.errors:
+            user = get_user(email=self.email.data)
+            user and self.email.errors.append(u'该邮箱已被注册')
+        
+        self.user = User(email=self.email.data, nickname=self.nickname.data, openids=[
+            UserOpenID(provider=session['openid_provider'], openid=session['current_openid'])])
+        self.user.set_password(self.password.data)
+        
+        return len(self.errors) == 0
+
+class UserInfoForm(wtf.Form):
+    nickname = wtf.TextField('nickname', validators=[
+        wtf.Required(message=u'请填写昵称')])
+    slug = wtf.TextField('slug', validators=[
+        wtf.Regexp(regex=r'[a-zA-Z0-9_-]{5,24}', message=u'域名为5到24位，由英文字母、数字或者符号(包括_-)组成')])
+    phone = wtf.TextField('phone', validators=[
+        wtf.Regexp(regex=r'^1\d{10}$', message=u'请输入有效的手机号码')])
+    phone_status = wtf.RadioField('phone_status', choices=[
+        (0, u'不公开'), (1, u'公开'), (2, u'仅向会员公开')])      
+    # photo = db.Column(db.String(255), nullable=True) # 存一张照片，既然有线下的聚会的，总得认得人才行
+    motoo = wtf.TextAreaField('motoo', validators=[
+        wtf.Length(min=0, max=255, message=u'座右铭最多为255个字符')])
+    introduction = wtf.TextAreaField('introduction', validators=[
+        wtf.Length(min=0, max=3000, message=u'个人介绍最多为3000个字')])
--- website/scriptfan/views/user.py
+++ website/scriptfan/views/user.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 from flask import Blueprint, request, session, url_for, redirect, render_template, flash, current_app as app
-from flask.ext import wtf, login 
+from flask.ext import wtf, login
 from flask.ext.login import current_user
 from scriptfan.extensions import db, oid, login_manager
 from scriptfan.models import get_user, User, UserOpenID
+from scriptfan.forms import *
+
 # import re
 
 userapp = Blueprint("user", __name__)
@@ -17,7 +19,7 @@ class LoginUser(login.UserMixin):
 
     def __init__(self, user):
         self.id = user.id
-        self.user = user 
+        self.user = user
 
 login_manager.anonymous_user = Anonymous
 login_manager.login_view = 'user.signin'
@@ -27,36 +29,6 @@ login_manager.login_message = u'需要登陆后才能访问本页'
 def load_user(user_id):
     user = User.query.get(user_id) 
     return user and LoginUser(user) or None
-
-class SigninForm(wtf.Form):
-    email = wtf.TextField('email', validators=[
-        wtf.Required(message=u'请填写电子邮件'), 
-        wtf.Email(message=u'无效的电子邮件')])
-    password = wtf.PasswordField('password', validators=[
-        wtf.Required(message=u'请填写密码'),
-        wtf.Length(min=5, max=20, message=u'密应应为5到20位字符')])
-    next = wtf.HiddenField('next')
-    remember = wtf.BooleanField('remember')
-    
-    openid_identifier = wtf.HiddenField('openid_identifier')
-    openid_provider = wtf.HiddenField('openid_provider')
-
-    def __init__(self, *args, **kargs):
-        wtf.Form.__init__(self, *args, **kargs)
-        self.user = None
-
-    def validate(self):
-        # 验证邮箱是否注册
-        if wtf.Form.validate(self):
-            user = get_user(email=self.email.data)
-            if not user:
-                self.email.errors.append(u'该邮箱尚未在本站注册')
-            elif not user.check_password(self.password.data):
-                self.password.errors.append(u'密码错误')
-            else:
-                self.user = user
-
-        return len(self.errors) == 0
 
 @userapp.route('/signin/', methods=['GET', 'POST'])
 @oid.loginhandler
@@ -95,39 +67,6 @@ def create_or_login(resp):
     return redirect(url_for('user.signup', next=oid.get_next_url(), 
         email=resp.email, nickname=resp.nickname or resp.fullname))
 
-class SignupForm(wtf.Form):
-    email = wtf.TextField('email', validators=[
-        wtf.Required(message=u'请填写电子邮件'), 
-        wtf.Email(message=u'无效的电子邮件')])
-    nickname = wtf.TextField('nickname', validators=[
-        wtf.Required(message=u'请填写昵称'),
-        wtf.Length(min=2, max=20, message=u'昵称应为2到20字符')])
-    password = wtf.PasswordField('password', validators=[
-        wtf.Required(message=u'请填写密码'),
-        wtf.Length(min=5, max=20, message=u'密码应为5到20位字符')])
-    repassword = wtf.PasswordField('repassword', validators=[
-        wtf.Required(message=u'请填写确认密码'),
-        wtf.EqualTo('password', message=u'两次输入的密码不一致')])
-    next = wtf.HiddenField('next')
-
-    def __init__(self, *args, **kargs):
-        wtf.Form.__init__(self, *args, **kargs)
-        self.user = None
-
-    def validate(self):
-        wtf.Form.validate(self)
-
-        # 验证邮箱是否注册
-        if not self.email.errors:
-            user = get_user(email=self.email.data)
-            user and self.email.errors.append(u'该邮箱已被注册')
-        
-        self.user = User(email=self.email.data, nickname=self.nickname.data, openids=[
-            UserOpenID(provider=session['openid_provider'], openid=session['current_openid'])])
-        self.user.set_password(self.password.data)
-        
-        return len(self.errors) == 0
-
 @userapp.route('/signup/', methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated():
@@ -151,21 +90,6 @@ def signup():
 def profile(slug=None, user_id=None):
     # TODO: 用户资料修改及密码修改
     return render_template('user/profile.html')
-
-class UserInfoForm(wtf.Form):
-    nickname = wtf.TextField('nickname', validators=[
-        wtf.Required(message=u'请填写昵称')])
-    slug = wtf.TextField('slug', validators=[
-        wtf.Regexp(regex=r'[a-zA-Z0-9_-]{5,24}', message=u'域名为5到24位，由英文字母、数字或者符号(包括_-)组成')])
-    phone = wtf.TextField('phone', validators=[
-        wtf.Regexp(regex=r'^1\d{10}$', message=u'请输入有效的手机号码')])
-    phone_status = wtf.RadioField('phone_status', choices=[
-        (0, u'不公开'), (1, u'公开'), (2, u'仅向会员公开')])      
-    # photo = db.Column(db.String(255), nullable=True) # 存一张照片，既然有线下的聚会的，总得认得人才行
-    motoo = wtf.TextAreaField('motoo', validators=[
-        wtf.Length(min=0, max=255, message=u'座右铭最多为255个字符')])
-    introduction = wtf.TextAreaField('introduction', validators=[
-        wtf.Length(min=0, max=3000, message=u'个人介绍最多为3000个字')])
 
 @userapp.route('/edit')
 @login.login_required
