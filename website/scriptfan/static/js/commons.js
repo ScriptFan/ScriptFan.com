b43fecda56e5cfaadf283d4ef81adfbff29e4aee
greatghoul <greatghoul@gmail.com>
2012-10-26 01:23:33 +0800

    添加公共js文件和bootstrap表格扩展

---
+++ website/scriptfan/static/js/commons.js
@@ -0,0 +1,13 @@
+function fmt() {
+    var args = arguments;
+    return args[0].replace(/%\{(.*?)}/g, function(match, prop) {
+        return function(obj, props) {
+            var prop = /\d+/.test(props[0]) ? parseInt(props[0]) : props[0];
+            if (props.length > 1) {
+                return arguments.callee(obj[prop], props.slice(1));
+            } else {
+                return obj[prop] || '';
+            }
+        }(typeof args[1] === 'object' ? args[1] : args, prop.split(/\.|\[|\]\[|\]\./));
+    });
+}
