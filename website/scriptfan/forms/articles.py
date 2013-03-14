c46e71e2f60c7dfee8c01187e2902c9d64157fa2
greatghoul <greatghoul@gmail.com>
2013-03-14 08:11:24 +0800

    #12 Add forms for article and category operations

---
+++ website/scriptfan/forms/articles.py
@@ -0,0 +1,28 @@
+#-*- coding: utf-8 -*-
+
+"""
+    scriptfan/forms/articles
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Forms for articles and categories
+"""
+
+from flask.ext import wtf
+from scriptfan.forms.base import RedirectForm
+
+class ArticleForm(RedirectForm):
+    """ Form for article create and update """
+
+    title = wtf.TextField('email', validators=[
+        wtf.Required(message=u'请填写标题')]
+    type = wtf.PasswordField('type', validators=[
+        wtf.Required(message=u'请选择类型')])
+    content = wtf.TextArea('content', validators=[wtf.Required(message=u'文章内容不能为空']))
+    # TODO: Fill article category dropdown with categories
+    category_id = wtf.SelectField('category_id')
+
+class CategoryForm(RedirectForm):
+    """ Form for category create and update """
+
+    name = wtf.TextField("name", validators=[wtf.Required(message=u'名称不能为空')])
+    name = wtf.TextField("slug", validators=[wtf.Required(message=u'名称不能为空')])
