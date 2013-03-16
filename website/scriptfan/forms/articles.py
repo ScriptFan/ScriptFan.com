1a3b760969f5807c3f2ef42084da6b36ff07cf1b
iMom0 <mobeiheart@gmail.com>
2013-03-16 22:54:12 +0800

    Add first working create article view

--- website/scriptfan/forms/articles.py
+++ website/scriptfan/forms/articles.py
@@ -3,20 +3,22 @@
 """
     scriptfan/forms/articles
     ~~~~~~~~~~~~~~~~~~~~~~~~
-    
+
     Forms for articles and categories
 """
 
 from flask.ext import wtf
 from scriptfan.forms.base import RedirectForm
 
+
 class ArticleForm(RedirectForm):
     """ Form for article create and update """
 
-    title = wtf.TextField('email', validators=[wtf.Required(message=u'请填写标题')])
+    title = wtf.TextField('title', validators=[wtf.Required(message=u'请填写标题')])
     content = wtf.TextAreaField('content', validators=[wtf.Required(message=u'文章内容不能为空')])
     # TODO: Fill article category dropdown with categories
-    category_id = wtf.SelectField('category_id')
+    # category_id = wtf.SelectField('category_id')
+
 
 class CategoryForm(RedirectForm):
     """ Form for category create and update """
